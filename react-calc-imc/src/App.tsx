f9964dd2495b6223bde3fa0531ebd3d2e5a49fc0
RafaelSdm <rafaeldamasceno2000@gmail.com>
2022-03-28 13:38:41 -0300

    criando estrurura base

--- react-calc-imc/src/App.tsx
+++ react-calc-imc/src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+import styles from './App.module.css'
+import logo from './assets/logo.png'
+
+const App = () =>{
+  return(
+    <div className={styles.main} >
+      <header>
+        <div className={styles.headerContainer}>
+          <img src={logo} alt=""/>
+        </div>
       </header>
+
+      <div className={styles.container}>
+
+        <div className={styles.left}>
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam mollitia reiciendis, maiores nam id animi qui suscipit quae dolores ab cum fugit velit at quidem accusantium! Veritatis accusantium similique illum?
+
+        </div>
+
+        <div className={styles.right}>
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur unde deleniti reiciendis magni, officia repudiandae nam qui quo? Sapiente pariatur reprehenderit natus consectetur placeat earum harum praesentium inventore est accusantium.
+
+        </div>
+
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
